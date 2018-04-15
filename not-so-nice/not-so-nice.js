7fe4a2d932ed8550361c1e012a5dfe2ee06b5c97
Elijah Grubb <u0894728@utah.edu>
2018-04-15 17:02:12 -0600

    bug fix?

--- not-so-nice/not-so-nice.js
+++ not-so-nice/not-so-nice.js
@@ -1,18 +1,22 @@
 !function() {
-	var me = Players.getMe();
-	var mount = Textures.sprite('mountain4');
-	game.graphics.layers.flags.addChild(mount);
+	var me;
+	var mount;
 
-	setInterval(function(){
-		mount.visible = false;
-		for (var playerID in Players.getIDs()) {
-			var player = Players.get(playerID);
-			if (player.type == 5 && player.team != me.team && Math.abs(player.lowResPos.x - me.lowResPos.x) < 1250 && Math.abs(player.lowResPos.y - me.lowResPos.y) < 700 ) {
-				mount.position.set(player.lowResPos.x, player.lowResPos.y);
-				mount.visible = true;
+	SWAM.on("gamePrep", function() {
+		me = Players.getMe();
+		mount = Textures.sprite('mountain4');
+		game.graphics.layers.flags.addChild(mount);
+		setInterval(function(){
+			mount.visible = false;
+			for (var playerID in Players.getIDs()) {
+				var player = Players.get(playerID);
+				if (player.type == 5 && player.team != me.team && Math.abs(player.lowResPos.x - me.lowResPos.x) < 1250 && Math.abs(player.lowResPos.y - me.lowResPos.y) < 700 ) {
+					mount.position.set(player.lowResPos.x, player.lowResPos.y);
+					mount.visible = true;
+				}
 			}
-		}
-	}, 1000);
+		}, 1000);
+	});
 
 	SWAM.registerExtension({
 		name: "not-so-nice",
