576845900b6a2db3947737f86f8032513ec60946
Elijah Grubb <u0894728@utah.edu>
2018-04-15 20:03:26 -0600

    trying to fix referencing issue part 3

--- not-so-nice/not-so-nice.js
+++ not-so-nice/not-so-nice.js
@@ -2,20 +2,20 @@
 	
 	SWAM.on("gamePrep", function() {
 		console.log("setting up!");
-		var meObj = Players.getMe();
 		var mountObj = Textures.sprite('mountain4');
 		game.graphics.layers.flags.addChild(mountObj);
-		setInterval(function(me, mount){
+		setInterval(function(mount){
 			console.log("REFRESH!!!");
 			mount.visible = false;
 			for (var playerID in Players.getIDs()) {
 				var player = Players.get(playerID);
+				var me = Players.getMe();
 				if (player.type == 5 && player.team != me.team && Math.abs(player.lowResPos.x - me.lowResPos.x) < 1250 && Math.abs(player.lowResPos.y - me.lowResPos.y) < 700 ) {
 					mount.position.set(player.lowResPos.x, player.lowResPos.y);
 					mount.visible = true;
 				}
 			}
-		}, 1000, meObj, mountObj);
+		}, 1000, mountObj);
 	});
 
 	SWAM.registerExtension({
