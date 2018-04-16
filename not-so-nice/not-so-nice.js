9674b2dce5917bd16a66b1f26cdfa105fc75c4e7
Elijah Grubb <u0894728@utah.edu>
2018-04-15 20:44:28 -0600

    moving things around

--- not-so-nice/not-so-nice.js
+++ not-so-nice/not-so-nice.js
@@ -1,25 +1,24 @@
 !function() {
-
-	var mount = Textures.sprite('mountain4');
-	var ticker = new PIXI.ticker.Ticker();
-	console.log("test");
-	ticker.stop();
-	ticker.add(function(delta) {
-		console.log("REFRESH!!!");
-		mount.visible = false;
-		for (var playerID in Players.getIDs()) {
-			var player = Players.get(playerID);
-			var me = Players.getMe();
-			if (player.type == 5 && player.team != me.team && Math.abs(player.lowResPos.x - me.lowResPos.x) < 1250 && Math.abs(player.lowResPos.y - me.lowResPos.y) < 700 ) {
-				mount.position.set(player.lowResPos.x, player.lowResPos.y);
-				mount.visible = true;
-			}
-		}
-	});
 	
 	SWAM.on("gamePrep", function() {
 		console.log("setting up!");
+		var mount = Textures.sprite('mountain4');
 		game.graphics.layers.flags.addChild(mount);
+		var ticker = new PIXI.ticker.Ticker();
+		console.log("test");
+		ticker.stop();
+		ticker.add(function(delta) {
+			console.log("REFRESH!!!");
+			mount.visible = false;
+			for (var playerID in Players.getIDs()) {
+				var player = Players.get(playerID);
+				var me = Players.getMe();
+				if (player.type == 5 && player.team != me.team && Math.abs(player.lowResPos.x - me.lowResPos.x) < 1250 && Math.abs(player.lowResPos.y - me.lowResPos.y) < 700 ) {
+					mount.position.set(player.lowResPos.x, player.lowResPos.y);
+					mount.visible = true;
+				}
+			}
+		});
 		ticker.start();
 	});
 
@@ -28,6 +27,6 @@
 		id: "notsonice",
 		description: "this is probably a bad idea",
 		author: "jello",
-		version: "0.1"
+		version: "0.2"
 	});
 }();
