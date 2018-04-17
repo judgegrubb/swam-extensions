56d4ef1be5c67861e1018de789911c08c9d42e62
Elijah Grubb <u0894728@utah.edu>
2018-04-16 19:06:35 -0600

    Revert "moving refresh loop to ticker"
    This reverts commit 0b410ed29500061f663fdd6e4045d6888673bb36.

--- not-so-nice/not-so-nice.js
+++ not-so-nice/not-so-nice.js
@@ -1,25 +1,21 @@
 !function() {
-
-	var mount = Textures.sprite('mountain4');
-	var ticker = new PIXI.ticker.Ticker();
-	ticker.stop();
-	ticker.add(function(delta) {
-		console.log("REFRESH!!!");
-		mount.visible = false;
-		for (var playerID in Players.getIDs()) {
-			var player = Players.get(playerID);
-			var me = Players.getMe();
-			if (player.type == 5 && player.team != me.team && Math.abs(player.lowResPos.x - me.lowResPos.x) < 1250 && Math.abs(player.lowResPos.y - me.lowResPos.y) < 700 ) {
-				mount.position.set(player.lowResPos.x, player.lowResPos.y);
-				mount.visible = true;
-			}
-		}
-	});
 	
 	SWAM.on("gamePrep", function() {
 		console.log("setting up!");
-		game.graphics.layers.flags.addChild(mount);
-		ticker.start();
+		var mountObj = Textures.sprite('mountain4');
+		game.graphics.layers.flags.addChild(mountObj);
+		setInterval(function(mount){
+			console.log("REFRESH!!!");
+			mount.visible = false;
+			for (var playerID in Players.getIDs()) {
+				var player = Players.get(playerID);
+				var me = Players.getMe();
+				if (player.type == 5 && player.team != me.team && Math.abs(player.lowResPos.x - me.lowResPos.x) < 1250 && Math.abs(player.lowResPos.y - me.lowResPos.y) < 700 ) {
+					mount.position.set(player.lowResPos.x, player.lowResPos.y);
+					mount.visible = true;
+				}
+			}
+		}, 1000, mountObj);
 	});
 
 	SWAM.registerExtension({
